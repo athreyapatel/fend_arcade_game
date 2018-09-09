1fdf91591697bafdb9de4a51cb38f1befea19829
Athreyapatel <athreyapatel@gmail.com>
2018-09-09 19:16:55 +0530

    Added things mentioned
    Started

--- js/app.js
+++ js/app.js
@@ -29,9 +29,9 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies =[];
 
-
-
+const player=new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
